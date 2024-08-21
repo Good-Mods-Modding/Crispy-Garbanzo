5d27490b4c8f35e9719a67df10e3472873ed7118
BeansBeefBroccoli <69123018+BeansBeefBroccoli@users.noreply.github.com>
2024-08-20 22:26:36 -0400

    So I changed the mod id...
    Also tags

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Crispy Garbanzo
 
-![Crispy Garbanzo Logo](src/main/templates/assets/crispygarbanzo/icon.png)  
+![Crispy Garbanzo Logo](src/main/templates/assets/crispy_garbanzo/icon.png)  
 crispy garbanzo
---
+++ src/main/java/org/beansbeefbroccoli/crispy_garbanzo/item/ModItems.java
@@ -0,0 +1,23 @@
+package org.beansbeefbroccoli.crispy_garbanzo.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.beansbeefbroccoli.crispy_garbanzo.CrispyGarbanzo;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CrispyGarbanzo.MOD_ID);
+
+    public static final DeferredItem<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_TUNGSTEN = ITEMS.register("raw_tungsten", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> TUNGSTEN_NUGGET = ITEMS.register("tungsten_nugget", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> BISMUTH_INGOT = ITEMS.register("bismuth_ingot", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> BISMUTH_NUGGET = ITEMS.register("bismuth_nugget", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
